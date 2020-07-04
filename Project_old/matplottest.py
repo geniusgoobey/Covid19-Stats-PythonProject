df096c72a6633e64449d477ed2cd2d202c984757
geniusgoobey <goobey@gmail.com>
2020-07-04 00:51:06 -0400

    mat

--- Project_old/matplottest.py
+++ Project_old/matplottest.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 plt.figure(figsize=(5,5))
+#plt.style.use('grayscale')
+test = "My Covid Stats for Country"
+plt.title(test)
+
 labels = ["USA","CHINA","INDIA","CANADA","Turkey"]
 values = [25,10,50,40,30]
-explode = [0.05,0.05,0.05,0.05,0.05]
-colors = ["c","b","g","r","y"]
+explode = [0.02,0.02,0.02,0.02,0.02]
+colors = ["lightcoral","cadetblue","lightseagreen","crimson","powderblue"]
 plt.pie(values, labels=labels, autopct = "%.1f%%",explode = explode, colors=colors)
 plt.show()
+
+print(plt.style.available)
